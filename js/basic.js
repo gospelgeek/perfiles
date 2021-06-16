c8d51394aa1bb8f3caa64ae69d3896d2c42fd500
Nataly Andrea Portillo Velasco <portillonataly29@gmail.com>
2021-06-16 18:51:16 -0500

    Ajustes de botones de paginación y preloader

--- js/basic.js
+++ js/basic.js
@@ -1,94 +1,144 @@
 /*
  * Basic sample
-*/
+ */
 
 function addPage(page, book) {
 
-	var id, pages = book.turn('pages');
+    var id, pages = book.turn('pages');
 
-	// Create a new element for this page
-	var element = $('<div />', {});
+    // Create a new element for this page
+    var element = $('<div />', {});
 
-	// Add the page to the flipbook
-	if (book.turn('addPage', element, page)) {
+    // Add the page to the flipbook
+    if (book.turn('addPage', element, page)) {
 
-		// Add the initial HTML
-		// It will contain a loader indicator and a gradient
-		element.html('<div class="gradient"></div><div class="loader"></div>');
+        // Add the initial HTML
+        // It will contain a loader indicator and a gradient
+        element.html('<div class="gradient"></div><div class="loader"></div>');
 
-		// Load the page
-		loadPage(page, element);
-	}
+        // Load the page
+        loadPage(page, element);
+    }
 
 }
 
 function loadPage(page, pageElement) {
 
-	// Create an image element
+    // Create an image element
 
-	var img = $('<img />');
+    var img = $('<img />');
 
-	img.mousedown(function(e) {
-		e.preventDefault();
-	});
+    img.mousedown(function(e) {
+        e.preventDefault();
+    });
 
-	img.load(function() {
-		
-		// Set the size
-		$(this).css({width: '100%', height: '100%'});
+    img.load(function() {
 
-		// Add the image to the page after loaded
+        // Set the size
+        $(this).css({ width: '100%', height: '100%' });
 
-		$(this).appendTo(pageElement);
+        // Add the image to the page after loaded
 
-		// Remove the loader indicator
-		
-		pageElement.find('.loader').remove();
-	});
+        $(this).appendTo(pageElement);
 
-	// Load the page
+        // Remove the loader indicator
 
-	img.attr('src', 'pages/' +  page + '.jpg');
+        pageElement.find('.loader').remove();
+    });
+
+    // Load the page
+
+    img.attr('src', 'pages/' + page + '.jpg');
 
 }
 
 
 function loadLargePage(page, pageElement) {
-	
-	var img = $('<img />');
 
-	img.load(function() {
+    var img = $('<img />');
 
-		var prevImg = pageElement.find('img');
-		$(this).css({width: '100%', height: '100%'});
-		$(this).appendTo(pageElement);
-		prevImg.remove();
-		
-	});
+    img.load(function() {
 
-	// Loadnew page
-	
-	img.attr('src', 'pages/' +  page + '-large.jpg');
+        var prevImg = pageElement.find('img');
+        $(this).css({ width: '100%', height: '100%' });
+        $(this).appendTo(pageElement);
+        prevImg.remove();
+
+    });
+
+    // Loadnew page
+
+    img.attr('src', 'pages/' + page + '-large.jpg');
 }
 
 
 function loadSmallPage(page, pageElement) {
-	
-	var img = pageElement.find('img');
 
-	img.css({width: '100%', height: '100%'});
+    var img = pageElement.find('img');
 
-	img.unbind('load');
-	// Loadnew page
+    img.css({ width: '100%', height: '100%' });
 
-	img.attr('src', 'pages/' +  page + '.jpg');
+    img.unbind('load');
+    // Loadnew page
+
+    img.attr('src', 'pages/' + page + '.jpg');
 }
-
-
 
 // http://code.google.com/p/chromium/issues/detail?id=128488
 function isChrome() {
 
-	return navigator.userAgent.indexOf('Chrome')!=-1;
+    return navigator.userAgent.indexOf('Chrome') != -1;
 
 }
+
+
+/*Events to book's pagination*/
+
+
+const prevBtn = document.querySelector("#prev-btn");
+const nextBtn = document.querySelector("#next-btn");
+const book = document.querySelector("#flipbook");
+
+prevBtn.addEventListener("click", goPrevPage);
+nextBtn.addEventListener("click", goNextPage);
+book.addEventListener("click", hiddenBtn);
+book.addEventListener("mouseover", hiddenBtn);
+
+//Method to go to the next page
+function goNextPage() {
+    $('#flipbook').turn("next");
+    hiddenBtn();
+}
+
+//Method to go to the previous page
+function goPrevPage() {
+    $('#flipbook').turn("previous");
+    hiddenBtn();
+}
+
+//Method to hide the button at the beginning and end of the book
+
+function hiddenBtn() {
+
+    if ($('#flipbook').turn("page") > 1) {
+        prevBtn.style.visibility = "visible";
+    } else {
+        prevBtn.style.visibility = "hidden";
+    }
+
+    if ($('#flipbook').turn("page") == $('#flipbook').turn("pages")) {
+        nextBtn.style.visibility = "hidden";
+    } else {
+        nextBtn.style.visibility = "visible";
+    }
+
+}
+
+/*Loader */
+
+$(window).on('load', function() {
+    setTimeout(function() {
+        $(".loader-page").css({ visibility: "hidden", opacity: "0" })
+    }, 1500);
+
+});
